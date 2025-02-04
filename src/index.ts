05f338e2a46568bcbec3e28317667a488217de6b
ratabb <ratabb@users.noreply.github.com>
2025-02-04 19:54:14 +0700

    workers-ai

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,34 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
+import { createWorkersAI } from "workers-ai-provider";
+import { generateText } from "ai";
 import { Bot, Context, webhookCallback } from "grammy";
 
 export interface Env {
-  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-  // MY_KV_NAMESPACE: KVNamespace;
-  //
-  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-  // MY_DURABLE_OBJECT: DurableObjectNamespace;
-  //
-  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-  // MY_BUCKET: R2Bucket;
-  //
-  // Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-  // MY_SERVICE: Fetcher;
-  //
-  // Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-  // MY_QUEUE: Queue;
   BOT_INFO: string;
   BOT_TOKEN: string;
+  AI: any;
 }
 
 export default {
   async fetch(
     request: Request,
     env: Env,
-    ctx: ExecutionContext,
+    ctx: ExecutionContext
   ): Promise<Response> {
     const bot = new Bot(env.BOT_TOKEN, { botInfo: JSON.parse(env.BOT_INFO) });
-
-    bot.command("start", async (ctx: Context) => {
-      await ctx.reply("Hello, world!");
+    const workersai = createWorkersAI({ binding: env.AI });
+    const model = workersai("@cf/meta/llama-2-7b-chat-int8");
+    bot.on("message", async (ctx: Context) => {
+      await ctx.replyWithChatAction("typing");
+      try {
+        const response = await generateText({
+          model: model,
+          prompt: ctx.message!.text!,
+        });
+        await ctx.reply(response.text);
+      } catch (err) {
+        await ctx.reply((err as Error).message);
+      }
     });
-
     return webhookCallback(bot, "cloudflare-mod")(request);
   },
 };
